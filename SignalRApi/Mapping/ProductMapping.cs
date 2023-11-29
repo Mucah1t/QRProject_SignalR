45e87eca5141db959557afd565f248a66fbfffe5
mtyilmaz58@outlook.com <mtyilmaz58@outlook.com>
2023-11-29 17:38:37 +0300

    Relations between Category and Product Tables have been established. The category name was reached by writing an entity-specific method.

--- SignalRApi/Mapping/ProductMapping.cs
+++ SignalRApi/Mapping/ProductMapping.cs
@@ -12,7 +12,8 @@ namespace SignalRApi.Mapping
             CreateMap<Product, ResultProductDto> ().ReverseMap();
             CreateMap<Product, CreateProductDto> ().ReverseMap();
             CreateMap<Product, GetProductDto> ().ReverseMap();
-            CreateMap<Product, UpdateProductDto> ().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+            CreateMap<Product, ResultProductWithCategory> ().ReverseMap();
         }
     }
 }
